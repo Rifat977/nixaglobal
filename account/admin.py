f949cd87b147185a0c1bdbdb77676ccb12c5b5c0
rifat977 <itscrifat5147@gmail.com>
2024-03-26 17:10:53 +0600

    notification

--- account/admin.py
+++ account/admin.py
@@ -21,8 +21,5 @@ admin.site.register(DocumentImage, NotAddableModelAdmin)
 class NotificationAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False 
-    
-    def has_add_permission(self, request):
-        return False 
 
 admin.site.register(Notification, NotificationAdmin)
